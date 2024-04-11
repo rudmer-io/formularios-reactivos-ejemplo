d2aceef4a86d0448d6f0fd86c22e207eee42be04
Rudmer <rudmermartinez@gmail.com>
2024-04-11 09:09:02 -0400

    Adding HttpClientModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { TestServiceService } from './services/test-service.service';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +11,57 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   myForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  /*  productos: any[] = []; */
+
+  productos = [
+    {
+      id: 1,
+      name: 'iPhone 13',
+      price: 1000,
+    },
+    {
+      id: 2,
+      name: 'iPhone 14',
+      price: 2000,
+    },
+    {
+      id: 3,
+      name: 'iPhone 15',
+      price: 3000,
+    },
+    {
+      id: 4,
+      name: 'iPhone 16',
+      price: 4000,
+    },
+    {
+      id: 5,
+      name: 'iPhone 17',
+      price: 5000,
+    },
+    {
+      id: 6,
+      name: 'iPhone 18',
+      price: 6000,
+    },
+  ];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private testService: TestServiceService
+  ) {
     this.myForm = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
     });
+  }
+
+  ngOnInit(): void {
+    this.getFromApi();
   }
 
   onSubmit() {
@@ -28,5 +71,9 @@ export class AppComponent {
     } else {
       console.log('Formulario inválido');
     }
+  }
+
+  getFromApi() {
+    this.testService.getFromApi().subscribe((data) => console.log(data));
   }
 }
